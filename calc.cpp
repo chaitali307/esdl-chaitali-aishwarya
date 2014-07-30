17e7656abe7e42955dbb23323bb65f3a7156dff9
chaitali307 <chaitali.pawbake@cumminscollege.in>
2014-07-30 14:40:42 +0530

    Update calc.cpp
    addition method added

--- calc.cpp
+++ calc.cpp
@@ -2,7 +2,8 @@
 void main()
 {
 cout<<"Welcome to GITHUB";
-cout<<"square"<<square(1.0);
+cout<<"square",<<square(1.0);
+cout<<"addition",<<add(4.0);
 }
 double square(double in_num)
 {
@@ -10,4 +11,12 @@ double square(double in_num)
   out_num=in_num*in_num;
   return out_num;
 }
+
+double add(double in_num)
+{
+  double out_name;
+  out_num=in_num+in_num;
+  return out_num;
+}
+
 }
